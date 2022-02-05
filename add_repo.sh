2ce9d08e803fc9b4d8b5e429a2340428b786cdc8
Andrey Ryoo <andrey.ryoo@gmail.com>
2022-02-05 15:21:09 +0300

    Fix

--- add_repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-
-echo "Adding Repo $3"
-repo add --username "$1" --password "$2" "$3" "$4"
--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-
-echo "Publishing Helm Chart $1"
-
-helm push-artifactory "$1" "$2" --access-token "$3" --skip-reindex
