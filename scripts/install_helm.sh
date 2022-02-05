a8e2db2e79a93cd7f7eb24bb8c29da3c4c6d35ac
Andrey Ryoo <andrey.ryoo@gmail.com>
2022-02-05 15:08:42 +0300

    Fix

--- scripts/install_helm.sh
+++ scripts/install_helm.sh
@@ -4,9 +4,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+echo "Installing Helm"
+
 curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+
 apt-get install -y apt-transport-https
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
 apt-get update
 apt-get install -y helm
+
 helm plugin install https://github.com/belitre/helm-push-artifactory-plugin --version v1.0.2
